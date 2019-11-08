b90b9312c1490346c8e6ff950ccd06ad7cfb3475
Naya  Willis <greedybrain18@gmail.com>
2019-11-08 20:50:51 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -25,19 +25,6 @@ class Student
     DB[:conn].execute(sql, name).map{|row| self.new_from_db(row)}.first
   end
   
-  # def self.find_by_name(name)
-  #   sql = <<-SQL
-  #     SELECT *
-  #     FROM songs
-  #     WHERE name = ?
-  #     LIMIT 1
-  #   SQL
- 
-  #   DB[:conn].execute(sql, name).map do |row|
-  #     self.new_from_db(row)
-  #   end.first
-  # end
-  
   def save
     sql = <<-SQL
       INSERT INTO students (name, grade) 
