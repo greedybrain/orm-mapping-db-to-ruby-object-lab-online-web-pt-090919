7dff7826b923eb075b0764b2055c136dad7aefb6
Naya  Willis <greedybrain18@gmail.com>
2019-11-08 21:11:36 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -49,10 +49,10 @@ def self.students_below_12th_grade
       SELECT *
       FROM students 
       WHERE grade = 10 
-      LIMIT ?
+      LIMIT #{amt}
     SQL
     
-    DB[:conn].execute(sql).map{|row| self.new_from_db(row)}[0..1]
+    DB[:conn].execute(sql).map{|row| self.new_from_db(row)}
   end
   
   def self.first_student_in_grade_10
