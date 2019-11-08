a965ace0fa7964ec51ff2ef74355c8d300040686
Naya  Willis <greedybrain18@gmail.com>
2019-11-08 20:50:08 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -25,18 +25,18 @@ class Student
     DB[:conn].execute(sql, name).map{|row| self.new_from_db(row)}.first
   end
   
-  def self.find_by_name(name)
-    sql = <<-SQL
-      SELECT *
-      FROM songs
-      WHERE name = ?
-      LIMIT 1
-    SQL
+  # def self.find_by_name(name)
+  #   sql = <<-SQL
+  #     SELECT *
+  #     FROM songs
+  #     WHERE name = ?
+  #     LIMIT 1
+  #   SQL
  
-    DB[:conn].execute(sql, name).map do |row|
-      self.new_from_db(row)
-    end.first
-  end
+  #   DB[:conn].execute(sql, name).map do |row|
+  #     self.new_from_db(row)
+  #   end.first
+  # end
   
   def save
     sql = <<-SQL
